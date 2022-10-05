ecb0e51d2c5b7e1d51f292b35cf0f22e0fcd9ac7
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-10-05 10:21:13 -0300

    configs

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front-end';
+
+  constructor(titulo: Title){
+    titulo.setTitle("Home | eAgenda");
+  }
 }
