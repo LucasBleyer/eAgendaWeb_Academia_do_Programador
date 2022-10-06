ac04c3d16f0e663d890e97f01ef84e653ba36b39
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-10-06 11:27:45 -0300

    html dashboard e module core

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistroComponent } from './auth/registro/registro.component';
 
+
 const routes: Routes = [
   { path: '', redirectTo: 'conta/registrar', pathMatch: 'full' },
-  { path: 'conta/registrar', component: RegistroComponent }
+  { path: 'conta/registrar', component: RegistroComponent },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module')
+      .then(m => m.DashboardModule)
+  }
 ];
 
 @NgModule({
